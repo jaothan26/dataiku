9c481b3a1bc967dd9022323067fa4fd4a430929a
admin <jaothan26@gmail.com>
2020-12-06 10:49:52 +0100

    Recipe TEST_LGT.compute_m saved through notebook notebook editor for compute_m

--- recipes/compute_m.py
+++ recipes/compute_m.py
@@ -1,20 +1,33 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
+from dataiku import Dataset, Folder
+import pandas as pd
+import json
+import requests
+import base64
+from pandas.io.json import json_normalize
 
 # Read recipe inputs
-clean_pos = dataiku.Dataset("clean_pos")
-clean_pos_df = clean_pos.get_dataframe()
+#data15_17_joined_author_data = dataiku.Dataset("data15_17_joined_author_data")
+#data15_17_joined_author_data_df = data15_17_joined_author_data.get_dataframe()
 
+# Create Pandas dataframe
+counter = 1
+freshdesk_companies_final = pd.DataFrame()
+tmp = True
+# Pull data using Freshdesk API using companies endpoint
+while tmp:
+    FRESHDESK_ENDPOINT = "https://newaccount160716329665.freshdesk.com" # check if you have configured https, modify accordingly
+    FRESHDESK_KEY = "OgEeDb5wzXsh9Ryt8DMM"
+    r = requests.get(FRESHDESK_ENDPOINT + '/api/v2/companies?page=' + str(counter) + '&per_page=100',auth=(FRESHDESK_KEY, "X"))
+    if r.content == '[]':
+        break
+    freshdesk_companies = pd.DataFrame(json_normalize(json.loads(r.content)))
+    freshdesk_companies_final = freshdesk_companies_final.append(freshdesk_companies)
+    print (counter)
+    counter += 1
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-m_df = clean_pos_df # For this sample code, simply copy input to output
-
-
-# Write recipe outputs
-m = dataiku.Dataset("m")
-m.write_with_schema(m_df)
+# Write resulting dataframe into the output dataset
+freshdesk_companies_list = dataiku.Dataset("Freshdesk_companies_list")
+freshdesk_companies_list.write_with_schema(freshdesk_companies_final)
